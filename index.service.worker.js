5430b3cd2a083ee470b581585b0d547f6515c274
Arcanauto AI <arcanauto-ai@users.noreply.github.com>
2026-09-04 02:31:35 +0900

    deploy: auto-update 2026-09-04

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788456490|4462537';
+const CACHE_VERSION = '1788456686|4525261';
 /** @type {string} */
 const CACHE_PREFIX = 'PocketCreatures-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
