0a006c359c40e80c8d2e9e1ba7d601a94dc155da
Arcanauto AI <arcanauto-ai@users.noreply.github.com>
2026-09-04 03:17:48 +0900

    deploy: v1.5.0 large scrolling map

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788459082|4772907';
+const CACHE_VERSION = '1788459455|4774614';
 /** @type {string} */
 const CACHE_PREFIX = 'PocketCreatures-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
